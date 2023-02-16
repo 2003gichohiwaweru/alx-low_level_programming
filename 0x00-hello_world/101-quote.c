ede5a929cc90e93ace6a5afede0b7b03859b1983
josephwaweru <josephgichohi322@example.com>
2023-02-16 12:58:34 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line followed by a new blank line using fprintf
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fprintf( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
