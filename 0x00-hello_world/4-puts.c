f68393c7fcc8cc6a4352599b9d4c413e31a7a574
josephwaweru <josephwaweru@example.com>
2023-02-16 11:44:41 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main -Aprogram that prints a new line with put function
+ * Return:0(Success)
+ */
+int main(void)
+{
+puts("/"Programming is like building a a multilingual puzzle");
+return(0);
+}
