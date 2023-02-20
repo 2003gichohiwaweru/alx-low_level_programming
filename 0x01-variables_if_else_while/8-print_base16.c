5a6ce6aa727033b17e73aa76e3265a0e7213feef
josephwawer <2003gichohiwaweru>
2023-02-20 08:19:04 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 /**
  * main  main block
- * Description: prints all single digit numbers of base 16
+ * Description: prints all single digit number of base 10
  * Starting from 0, followed by a new line.
- * Return: 0
+ * Return: always 0 (Success)
  */
 int main(void)
 {
