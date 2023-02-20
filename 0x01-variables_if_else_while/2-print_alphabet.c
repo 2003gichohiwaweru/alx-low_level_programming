8972dea4fc2607d759a5dc266cd8f6781ae3491e
josephwawer <2003gichohiwaweru>
2023-02-20 05:58:43 -0800

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * prints the alphabets 
+ * Return always (Success)
+ */
+
+int main (void)
+{
+	char c;
+
+	for (c ='a'; c<= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
