dcb88a31297875628a0769d838f0fbc670870f35
josephwawer <2003gichohiwaweru>
2023-02-21 10:12:46 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * prin_alphabet_x10 -Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (a = 'b'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
