6ab8cae47a164b848480a1dd4aec36fade6e538f
josephwawer <2003gichohiwaweru>
2023-02-21 13:56:06 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int_add - Adds two intergers
+ * Returns:The results
+ */
+
+int add(int b, int a)
+{
+	int  s;
+
+	_putchar(a);
+	_putchar(b);
+	s = a + b;
+	_putchar('\n');
+	return (s);
+}
