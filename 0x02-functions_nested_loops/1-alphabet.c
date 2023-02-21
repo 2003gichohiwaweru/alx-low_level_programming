3f28ec0e86c665bd5c41a126a2aff0bf7b415b60
josephwawer <2003gichohiwaweru>
2023-02-21 09:46:38 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet -Make the alphabet
+ * Return: void
+ * prints in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'b'; a <= 'z'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
