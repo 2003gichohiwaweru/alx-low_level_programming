8bd401eb1d0be3483617dc183e18a90f2710509f
joseph <josephgichohiwaweru@gmail.com>
2023-04-10 04:31:14 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,42 +3,41 @@
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
- * 
+ *
  * @filename: A pointer to the name of the file.
  *
  * @letters: The number of letters the
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         Op/wr - the actual number of bytes the function can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t rd, wr, op;
-	char *buff;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	op = open(filename, O_RDONLY);
-	rd = read(op, buff, letters);
-	wr = write(STDOUT_FILENO, buff, rd);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (op == -1 || rd == -1 || wr != rd)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buff);
-		return  (0);
-
+		free(buffer);
+		return (0);
 	}
 
-	free(buff);
-	close(0);
+	free(buffer);
+	close(o);
 
-	return (wr);
-
+	return (w);
 }
