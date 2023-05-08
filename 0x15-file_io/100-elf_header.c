87463afadb9272aed82704b68a29a67d53fc3530
Joseph Waweru <josephgicho322@gmail.com>
2023-05-08 11:04:53 -0700

    void print_abi(unsigned char *e_ident); void print_osabi(unsigned char *e_ident); void print_type(unsigned int e_type, unsigned char *e_ident); void print_entry(unsigned long int e_entry, unsigned char *e_ident);

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,16 +1,4 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
+#include "main.h"
 
 
 /**
